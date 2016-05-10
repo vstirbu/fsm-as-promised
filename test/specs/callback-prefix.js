4de88f81058611e0e8a5aac0deda8a39a1f60cba
Vlad Stirbu <vstirbu@gmail.com>
2016-05-10 08:24:44 +0300

    expanded test

--- test/specs/callback-prefix.js
+++ test/specs/callback-prefix.js
@@ -6,14 +6,15 @@ module.exports = function (promise) {
       
       before(function () {
         prefix = StateMachine.callbackPrefix;
-        StateMachine.callbackPrefix = 'prefix';
       });
       
       after(function () {
         StateMachine.callbackPrefix = prefix;
       });
 
-      it('should call callbacks', function (done) {
+      it('should call callbacks with custom prefix', function (done) {
+        StateMachine.callbackPrefix = 'prefix';
+        
         var callbacks = [];
 
         var fsm = StateMachine({
@@ -36,6 +37,52 @@ module.exports = function (promise) {
               callbacks.push('enter');
             },
             prefixenteredthere: function () {
+              callbacks.push('entered');
+            }
+          }
+        });
+
+        fsm.jump()
+          .then(function () {
+            expect(callbacks)
+              .to
+              .be
+              .deep
+              .equal([
+                'leave',
+                'event',
+                'enter',
+                'entered'
+              ]);
+            done();
+          });
+      });
+
+      it('should call callbacks with no prefix', function (done) {
+        StateMachine.callbackPrefix = '';
+        
+        var callbacks = [];
+
+        var fsm = StateMachine({
+          initial: 'here',
+          events: [
+            {
+              name: 'jump',
+              from: 'here',
+              to: 'there'
+            }
+          ],
+          callbacks: {
+            leavehere: function () {
+              callbacks.push('leave');
+            },
+            jump: function () {
+              callbacks.push('event');
+            },
+            enterthere: function () {
+              callbacks.push('enter');
+            },
+            enteredthere: function () {
               callbacks.push('entered');
             }
           }
