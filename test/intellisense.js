fbd0f86ff967df438384bc82fff7b0f613cbd840
Vlad Stirbu <vstirbu@gmail.com>
2017-10-27 17:56:53 +0300

    test type definitions using chai-as-typed

--- test/intellisense.js
+++ test/intellisense.js
@@ -1,24 +1,16 @@
 /* global global */
 
 var expect = require('chai').expect;
+var chai = require('chai');
+var chaiAsTyped = require('chai-as-typed');
 var ts = require('typescript');
+
+chai.use(chaiAsTyped);
 
 describe('Intellisense', function () {
   it('should not give errors', function () {
     this.timeout(5000);
 
-    const options = {
-      allowJs: true,
-      noEmit: true,
-      target: ts.ScriptTarget.ES5,
-      module: ts.ModuleKind.CommonJS,
-      moduleResolution: ts.ModuleResolutionKind.NodeJs
-    };
-    const program = ts.createProgram([__dirname + '/intellisense/fsm.js'], options);
-    const results = program.emit();
-
-    const diagnostics = ts.getPreEmitDiagnostics(program).concat(results.diagnostics);
-
-    expect(diagnostics).to.have.length(0);
+    expect(__dirname + '/intellisense/fsm.js').to.have.types.validated();
   });
 });
